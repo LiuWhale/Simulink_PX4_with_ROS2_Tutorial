6d0ea36062567fc8dbb41ef3a1448fd91c8969dc
LiuWhale <abc541550872@live.com>
2022-05-10 13:15:41 +0800

    [传说中的羊咩咩] Add NO1_ArmQuadcopter model.

--- utilities/launchArmQuadcopterModel.m
+++ utilities/launchArmQuadcopterModel.m
@@ -1,9 +1,9 @@
-function launchReaduORBDataModel
+function launchArmQuadcopterModel
 %
 
 %   Copyright 2021 The MathWorks, Inc.
 
     project = simulinkproject;
     projectRoot = project.RootFolder;
-    open_system(fullfile(projectRoot,'models','No1_ReadUORBData.slx'));
+    open_system(fullfile(projectRoot,'models','NO1_ArmQuadcopter.slx'));
 end
